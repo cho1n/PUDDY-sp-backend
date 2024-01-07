b2e720903e805bde439d2bd0ddb4c235d1de2245
JeongHeumChoi <andyc707@gmail.com>
2024-01-08 02:48:29 +0900

    feat : Person 정보 조회 API 구현

--- src/main/java/sideproject/puddy/controller/PersonController.java
+++ src/main/java/sideproject/puddy/controller/PersonController.java
@@ -2,8 +2,12 @@ package sideproject.puddy.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sideproject.puddy.dto.person.response.PersonInfoResponse;
+import sideproject.puddy.service.PersonService;
 
 
 @Slf4j
@@ -11,5 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class PersonController {
+    private final PersonService personService;
+
+    @GetMapping("/person")
+    public ResponseEntity<PersonInfoResponse> getPersonInfo() {
+        final PersonInfoResponse myInformation = personService.findPersonInfo();
+        return ResponseEntity.ok(myInformation);
+    }
 
 }
--- src/main/java/sideproject/puddy/service/PersonService.java
+++ src/main/java/sideproject/puddy/service/PersonService.java
@@ -3,10 +3,27 @@ package sideproject.puddy.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import sideproject.puddy.dto.person.response.PersonInfoResponse;
+import sideproject.puddy.model.Person;
+import sideproject.puddy.security.util.SecurityUtil;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class PersonService {
+
+    private final AuthService authService;
+
+    public PersonInfoResponse findPersonInfo() {
+        Person person = authService.findById(SecurityUtil.getCurrentUserId());
+        return PersonInfoResponse.of(
+                person.getLogin(),
+                person.getPassword(),
+                person.getBirth(),
+                person.getMainAddress(),
+                person.getSubAddress(),
+                person.isGender()
+        );
+    }
 
 }
