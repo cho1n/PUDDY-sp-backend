2e2063ffd0a0c0bfd8b76328f987e567d6e0294c
Github <alswkdrb@dgu.ac.kr>
2024-01-09 11:42:44 +0900

    feat : post dto 추가

---
+++ src/main/java/sideproject/puddy/dto/post/response/PostDetailResponse.java
@@ -0,0 +1,31 @@
+package sideproject.puddy.dto.post.response;
+
+import lombok.Data;
+import sideproject.puddy.dto.person.response.PersonProfileDto;
+
+import java.util.List;
+
+@Data
+public class PostDetailResponse {
+    private Long id;
+    private PersonProfileDto person;
+    private String title;
+    private String content;
+    private String createdAt;
+    private Like like;
+    private List<Comment> comments;
+    private boolean isMine;
+}
+
+@Data
+class Like {
+    private boolean isMine;
+}
+
+@Data
+class Comment {
+    private PersonProfileDto person;
+    private String content;
+    private String createdAt;
+    private boolean isMine;
+}
---
+++ src/main/java/sideproject/puddy/dto/post/response/PostListResponse.java
@@ -0,0 +1,24 @@
+package sideproject.puddy.dto.post.response;
+
+import lombok.Data;
+import sideproject.puddy.dto.person.response.PersonProfileDto;
+
+import java.util.List;
+
+@Data
+public class PostListResponse {
+    private Long count;
+    private List<PostResponse> postResponses;
+}
+
+@Data
+class PostResponse {
+    private Long id;
+    private PersonProfileDto person;
+    private String title;
+    private String content;
+    private String createdAt;
+    private Integer likeCount;
+    private Integer commentCount;
+    private boolean isMine;
+}
