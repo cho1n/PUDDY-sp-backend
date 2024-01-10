c6b08faf2a0d56a4ecbe937b5f999db47e410524
Github <alswkdrb@dgu.ac.kr>
2024-01-10 22:23:36 +0900

    feat : readPost 구현

--- src/main/java/sideproject/puddy/controller/PostController.java
+++ src/main/java/sideproject/puddy/controller/PostController.java
@@ -2,11 +2,17 @@ package sideproject.puddy.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sideproject.puddy.dto.post.request.PostRequest;
 import sideproject.puddy.dto.post.response.PostDetailResponse;
+import sideproject.puddy.dto.post.response.PostListResponse;
+import sideproject.puddy.model.Post;
 import sideproject.puddy.service.PostService;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -17,13 +23,8 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("/post")
-    public ResponseEntity<String> createPost(@RequestBody PostRequest postCreateRequest) {
-        return postService.savePost(postCreateRequest);
-    }
-
-    @DeleteMapping("/post/{postId}")
-    public ResponseEntity<String> deletePost(@PathVariable Long postId) {
-        return postService.deletePost(postId);
+    public ResponseEntity<String> createPost(@RequestBody PostRequest createRequest) {
+        return postService.savePost(createRequest);
     }
 
     @PatchMapping("/post/{postId}")
@@ -31,4 +32,19 @@ public class PostController {
         return postService.updatePost(updateRequest, postId);
     }
 
+    @DeleteMapping("/post/{postId}")
+    public ResponseEntity<String> deletePost(@PathVariable Long postId) {
+        return postService.deletePost(postId);
+    }
+
+    @GetMapping("/post/{postId}")
+    public ResponseEntity<PostDetailResponse> readPost(@PathVariable Long postId) {
+        return postService.readPost(postId);
+    }
+
+    @GetMapping("/post")
+    public ResponseEntity<List<Post>> pageList(@RequestParam int pageNum){
+        PageRequest pageRequest = PageRequest.of(pageNum - 1, 10);
+        return postService.postList(pageRequest, pageNum);
+    }
 }
--- src/main/java/sideproject/puddy/service/PostService.java
+++ src/main/java/sideproject/puddy/service/PostService.java
@@ -2,17 +2,24 @@ package sideproject.puddy.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sideproject.puddy.dto.comment.response.CommentDto;
+import sideproject.puddy.dto.person.response.PersonProfileDto;
 import sideproject.puddy.dto.post.request.PostRequest;
 import sideproject.puddy.dto.post.response.PostDetailResponse;
+import sideproject.puddy.dto.post.response.PostListResponse;
 import sideproject.puddy.exception.CustomException;
 import sideproject.puddy.exception.ErrorCode;
 import sideproject.puddy.model.Person;
 import sideproject.puddy.model.Post;
+import sideproject.puddy.repository.PostLikeRepository;
 import sideproject.puddy.repository.PostRepository;
 import sideproject.puddy.security.util.SecurityUtil;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -21,20 +28,13 @@ public class PostService {
 
     private final PostRepository postRepository;
     private final AuthService authService;
+    private final DogService dogService;
+    private final PostLikeRepository postLikeRepository;
 
     @Transactional
-    public ResponseEntity<String> savePost(PostRequest postCreateRequest) {
+    public ResponseEntity<String> savePost(PostRequest createRequest) {
         Person person = authService.findById(SecurityUtil.getCurrentUserId());
-        postRepository.save(new Post(postCreateRequest.getTitle(), postCreateRequest.getContent(), person));
-        return ResponseEntity.ok().body("ok");
-    }
-
-    @Transactional
-    public ResponseEntity<String> deletePost(Long postId) {
-        Person person = authService.findById(SecurityUtil.getCurrentUserId());
-        Post post = postRepository.findById(postId).orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
-        if(!post.getPerson().equals(person)) throw new CustomException(ErrorCode.USER_NOT_MATCH);
-        postRepository.delete(post);
+        postRepository.save(new Post(createRequest.getTitle(), createRequest.getContent(), person));
         return ResponseEntity.ok().body("ok");
     }
 
@@ -42,10 +42,49 @@ public class PostService {
     public ResponseEntity<String> updatePost(PostRequest updateRequest, Long postId) {
         Person person = authService.findById(SecurityUtil.getCurrentUserId());
         Post post = postRepository.findById(postId).orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
-        if(!post.getPerson().equals(person)) throw new CustomException(ErrorCode.USER_NOT_MATCH);
         post.updatePost(updateRequest.getTitle(), updateRequest.getContent());
         return ResponseEntity.ok().body("ok");
     }
 
+    @Transactional
+    public ResponseEntity<String> deletePost(Long postId) {
+        Person person = authService.findById(SecurityUtil.getCurrentUserId());
+        Post post = postRepository.findById(postId).orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+        postRepository.delete(post);
+        return ResponseEntity.ok().body("ok");
+    }
 
+    public ResponseEntity<PostDetailResponse> readPost(Long postId) {
+        Person person = authService.findById(SecurityUtil.getCurrentUserId());
+        Post post = postRepository.findById(postId).orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+
+        List<CommentDto> commentList = post.getComments().stream()
+                .map(comment -> CommentDto.builder()
+                        .person(new PersonProfileDto(comment.getPerson().isGender(), dogService.findByPersonAndMain(comment.getPerson())))
+                        .content(comment.getContent())
+                        .createdAt(comment.getCreatedAt().toString())
+                        .isMine(comment.getPerson().equals(person))
+                        .build()
+                )
+                .toList();
+
+        PostDetailResponse postDetailResponse = PostDetailResponse.builder()
+                .id(post.getId())
+                .person(new PersonProfileDto(post.getPerson().isGender(), dogService.findByPersonAndMain(post.getPerson())))
+                .title(post.getTitle())
+                .content(post.getContent())
+                .createdAt(post.getCreatedAt().toString())
+                .isLike(postLikeRepository.existsByPostAndPerson(post, person))
+                .likeCount(post.getPostLikes().size())
+                .comments(commentList)
+                .isMine(post.getPerson().equals(person))
+                .build();
+
+        return ResponseEntity.ok().body(postDetailResponse);
+    }
+
+    public ResponseEntity<List<Post>> postList(PageRequest pageRequest, int pageNum) {
+        List<Post> postList = postRepository.findAll(pageRequest).getContent();
+        return ResponseEntity.ok().body(postList);
+    }
 }
